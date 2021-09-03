fd41b5de9d1e7d89a6c66676106100e99d741951
Leonard Ehrenfried <mail@leonard.io>
2021-09-03 14:47:28 +0200

    Allow downloading zipped graph files before starting main process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,17 @@
-#! /bin/bash
+#! /bin/bash -e
+
+GRAPH_DIR=/opt/opentripplanner/graph/
+GRAPH_OUTPUT_FILE=${GRAPH_DIR}graph.obj
+ZIP_OUTPUT_FILE=${GRAPH_DIR}graph.zip
+
+if [[ ! -z "${GRAPH_ZIP_URL}" ]]; then
+  if [[ ! -f "$GRAPH_OUTPUT_FILE" ]]; then
+    curl --fail-with-body --create-dirs ${GRAPH_ZIP_URL} -o ${ZIP_OUTPUT_FILE}
+    unzip ${ZIP_OUTPUT_FILE} -d ${GRAPH_DIR}
+  else
+    echo "Graph file ${GRAPH_OUTPUT_FILE} already exists in container. Not downloading from ${GRAPH_ZIP_URL}"
+    exit 1
+  fi
+fi
+
 java $JAVA_OPTS -jar otp-shaded.jar $@
