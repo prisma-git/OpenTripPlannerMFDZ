cad4b86d7793d53b992eae2841df4599a3eaf5c7
Leonard Ehrenfried <mail@leonard.io>
2021-09-03 15:58:50 +0200

    Also start up the OTP process after downloading graph zip

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,13 @@ if [[ ! -z "${GRAPH_ZIP_URL}" ]]; then
   if [[ ! -f "$GRAPH_OUTPUT_FILE" ]]; then
     curl --fail-with-body --create-dirs ${GRAPH_ZIP_URL} -o ${ZIP_OUTPUT_FILE}
     unzip ${ZIP_OUTPUT_FILE} -d ${GRAPH_DIR}
+    java $JAVA_OPTS -jar otp-shaded.jar --load --serve ${GRAPH_DIR}
   else
     echo "Graph file ${GRAPH_OUTPUT_FILE} already exists in container. Not downloading from ${GRAPH_ZIP_URL}"
     exit 1
   fi
+else
+  echo "Environment variable GRAPH_ZIP_URL not set. Starting in manual mode."
+  java $JAVA_OPTS -jar otp-shaded.jar $@
 fi
 
-java $JAVA_OPTS -jar otp-shaded.jar $@
