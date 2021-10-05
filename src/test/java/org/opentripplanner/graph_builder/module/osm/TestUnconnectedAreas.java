86db98aeedb0b8288a8ff986254d845576fbb1e8
Leonard Ehrenfried <mail@leonard.io>
2021-10-05 16:52:37 +0200

    Update test

--- src/test/java/org/opentripplanner/graph_builder/module/osm/TestUnconnectedAreas.java
+++ src/test/java/org/opentripplanner/graph_builder/module/osm/TestUnconnectedAreas.java
@@ -40,9 +40,9 @@ public class TestUnconnectedAreas {
       int nParkAndRideLink = gg.getEdgesOfType(StreetVehicleParkingLink.class).size();
       int nParkAndRideEdge = gg.getEdgesOfType(VehicleParkingEdge.class).size();
 
-      assertEquals(11, nParkAndRide);
-      assertEquals(30, nParkAndRideLink);
-      assertEquals(41, nParkAndRideEdge);
+      assertEquals(12, nParkAndRide);
+      assertEquals(38, nParkAndRideLink);
+      assertEquals(42, nParkAndRideEdge);
     }
 
     @Test
@@ -57,9 +57,10 @@ public class TestUnconnectedAreas {
         int nParkAndRideLink = gg.getEdgesOfType(StreetVehicleParkingLink.class).size();
         int nParkAndRideEdge = gg.getEdgesOfType(VehicleParkingEdge.class).size();
 
-        assertEquals(11, nParkAndRideEntrances);
-        assertEquals(24, nParkAndRideLink);
-        assertEquals(31, nParkAndRideEdge);
+        assertEquals(13, nParkAndRideEntrances);
+        assertEquals(32, nParkAndRideLink);
+        assertEquals(33, nParkAndRideEdge);
+        
     }
 
     /**
