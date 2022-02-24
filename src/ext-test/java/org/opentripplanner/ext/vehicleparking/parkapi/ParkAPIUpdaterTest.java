e02dc485569eb8cec3ad3eabb376f46ce6271bcc
Leonard Ehrenfried <mail@leonard.io>
2022-02-24 12:18:33 +0100

    Add ParkAPI test für Ludwigsburg

--- src/ext-test/java/org/opentripplanner/ext/vehicleparking/parkapi/ParkAPIUpdaterTest.java
+++ src/ext-test/java/org/opentripplanner/ext/vehicleparking/parkapi/ParkAPIUpdaterTest.java
@@ -26,7 +26,10 @@ public class ParkAPIUpdaterTest {
 
         var first = parkingLots.get(0);
         assertEquals("Parkplatz Alenberghalle", first.getName().toString());
-        assertEquals(OsmOpeningHours.parseFromOsm("Mo-Su 00:00-24:00; PH 00:00-24:00"), first.getOpeningHours());
+        assertEquals(
+                OsmOpeningHours.parseFromOsm("Mo-Su 00:00-24:00; PH 00:00-24:00"),
+                first.getOpeningHours()
+        );
 
         assertTrue(first.hasAnyCarPlaces());
         assertNull(first.getCapacity());
@@ -38,4 +41,27 @@ public class ParkAPIUpdaterTest {
         assertEquals(1, last.getCapacity().getWheelchairAccessibleCarSpaces());
     }
 
+    @Test
+    void parseLudwigsburg() throws OpeningHoursParseException {
+        var url = "file:src/ext-test/resources/vehicleparking/parkapi/ludwigsburg.json";
+
+        var parameters =
+                new ParkAPIUpdaterParameters("", url, "park-api", 30, null, List.of(), null);
+        var updater = new CarParkAPIUpdater(parameters);
+
+        assertTrue(updater.update());
+        var parkingLots = updater.getUpdates();
+
+        assertEquals(18, parkingLots.size());
+
+        var first = parkingLots.get(0);
+        assertEquals("Forum/Blühendes Barock Ost", first.getName().toString());
+        assertEquals(
+                "Su 00:00-23:59; Mo 00:00-23:59; Tu 00:00-23:59; We 00:00-23:59; Th 00:00-23:59; Fr 00:00-23:59; Sa 00:00-23:59",
+                first.getOpeningHours().toString()
+        );
+
+        assertTrue(first.hasAnyCarPlaces());
+        assertEquals(158, first.getCapacity().getCarSpaces());
+    }
 }
